c89e4727ad77514e244ab6892c31ce99450e639a
Dora Gil <github@doragil.me>
2019-10-18 00:02:28 +0000

    drill 2

---
+++ drill2.js
@@ -0,0 +1,13 @@
+// 2. Even or odd
+
+// What is the Big O of the following algorithm? Explain your answer
+
+function isEven(value) {
+  if (value % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// * O(1) - Because it is constant and does not change the number of operations run regardless of input.
